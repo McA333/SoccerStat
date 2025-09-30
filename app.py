b9cba90bde65cf552d8e8dd48aa8c08678119be8
Nehemie-KM <nehemie.kakesa-muyeni@epitech.digital>
2025-09-30 10:35:40 +0200

    dépenndance

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Mon Projet Streamlit",
+    page_icon="🌍",
+    layout="wide"
+)
+
+st.title("🌍 Mon projet Streamlit")
+st.write("Bienvenue sur mon application !")
+
+st.sidebar.success("Sélectionne une page dans le menu ➡️")
